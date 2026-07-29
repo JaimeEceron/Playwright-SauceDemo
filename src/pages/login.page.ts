a706d903b48e0f8c8e76d6cbd76576a83aaa92fe
JaimeEceron <jimmyceron03@gmail.com>
2026-07-28 20:16:42 -0600

    Configuracion y Login exitoso

---
+++ src/pages/login.page.ts
@@ -0,0 +1,32 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+    
+  readonly page: Page;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    // Identificamos los elementos usando atributos data-test de SauceDemo
+    this.usernameInput = page.locator('[data-test="username"]');
+    this.passwordInput = page.locator('[data-test="password"]');
+    this.loginButton = page.locator('[data-test="login-button"]');
+  }
+
+  async navigateToLoginPage() {
+    await this.page.goto('/');
+  }
+
+  async loginWithEnvCredentials() {
+    
+    const username = process.env.SAUCE_USERNAME || '';
+    const password = process.env.SAUCE_PASSWORD || '';
+
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
--- tests/practicas.spec.ts
+++ tests/practicas.spec.ts
@@ -1,13 +1,27 @@
 import { test, Browser, Page, expect } from '@playwright/test';
+import { LoginPage } from '../src/pages/login.page';
+import { spec } from 'node:test/reporters';
  
- 
+ test.describe("Automatizacion en SauceDemo", () => {
+
   test("SauceDemo", async ({ page }) => {
 
-    await test.step("Empezamos a automatizar ", async () => {
+    await test.step("Configuramos LoginPage ", async () => {
 
-      await page.goto("https://www.saucedemo.com/")
+      const loginPage = new LoginPage(page);
 
-      
-    })
+      await test.step("Abrir navegador en el Login", async () => {
+      await loginPage.navigateToLoginPage();
+      await loginPage.loginWithEnvCredentials();
 
+      await expect(page).toHaveURL(/.*inventory*/);
+      await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+
+
+
+    });
+
+    });
+});
 });
